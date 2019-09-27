166af9576dc24d098eb1a7251152d031ef401cc3
abtolentino2 <52274229+abtolentino2@users.noreply.github.com>
2019-09-27 09:36:51 +0800

    Add files via upload

---
+++ Tolentino_Update/activities.php
@@ -0,0 +1,407 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title></title>
+
+  <!-- CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href="css/nivo-lightbox.css" rel="stylesheet" />
+  <link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+  <link href="css/owl.carousel.css" rel="stylesheet" media="screen" />
+  <link href="css/owl.theme.css" rel="stylesheet" media="screen" />
+  <link href="css/animate.css" rel="stylesheet" />
+  <link href="css/style.css" rel="stylesheet">
+  <link href="color/default.css" rel="stylesheet">
+  <!-- =======================================================
+    Theme Name: Lonely
+    Theme URL: https://bootstrapmade.com/free-html-bootstrap-template-lonely/
+    Author: BootstrapMade
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+  <!-- Preloader -->
+  <div id="preloader">
+    <div id="load"></div>
+  </div>
+
+  <!-- Section: intro -->
+  <section id="intro" class="intro">
+
+    <div class="slogan">
+      <a href="index.html"><img src="img/1.png" alt="" /></a>
+    </div>
+    <div class="page-scroll">
+      <a href="#about">
+				<i class="fa fa-angle-down fa-5x animated"></i>
+			</a>
+    </div>
+  </section>
+  <!-- /Section: intro -->
+
+  <!-- Navigation -->
+  <div id="navigation">
+    <nav class="navbar navbar-custom" role="navigation">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+                                                <i class="fa fa-bars"></i>
+                                                </button>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="menu">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="index.html">Home</a></li>
+                <li><a href="activities.html">Activities</a></li>
+                <li><a href="learnings.html">Learnings</a></li>
+				<li><a href="gallery.html">Gallery</a></li>
+                <li><a href="reflections.html">Reflections</a></li>
+				<li><a href="projects.html">Project</a></li>
+              </ul>
+            </div>
+            <!-- /.Navbar-collapse -->
+
+          </div>
+        </div>
+      </div>
+      <!-- /.container -->
+    </nav>
+  </div>
+  <!-- /Navigation -->
+
+  <!-- Section: about -->
+  <section id="about" class="home-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+
+          <div class="section-heading text-center">
+            <div class="wow bounceInDown" data-wow-delay="0.2s">
+              <h2>Anzel Clarenz Tolentino</h2>
+            </div>
+            <p class="wow bounceInUp" data-wow-delay="0.3s"><u>BSIT - MI181</u></p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+
+          <img src="img/Pic.jpg" class="img-responsive img-rounded" alt="" />
+        </div>
+        <div class="col-md-6">
+          <p><strong>Hello! I am Anzel Clarenz Tolentino, student from Asia Pacific College. </strong></p>
+          <p class = "act">
+Originally from the city of Pasay. Graduated Science, Technology, Engineering, and Mathematics strand on
+Senior High School also in Asia Pacific College. My capabilities as a student is not the high caliber type, mainly because
+I mostly do what I want to do other than having a fully academical mindset. Some would say that it's mediocre and irresponsible but I disagree,
+because I know my limitations and I always believed that it's always important to be balanced. 
+By balanced I mean it always depends of which environment I am with. If I was in home I would prioritize my family, If I was around my friend obviously I would socialize
+and if I was in school then I would think academically. 
+Choosing IT course is one of my biggest risk. Given that my interest in technology is highly curious, Information Technology course is one of the most difficult but in demand in the society, 
+by the reason of feeding my curiousity and seeking opportunity I took the biggest risk of my life and now I may be struggling but I know it would be worth it.
+
+In terms of personal life. I do nothing much as a hobby, I'm not sporty despite my physical potential, therefore I'm more likely in front of the computer to entertain myself. Sometimes I read fictional books
+when there are no internet or electricity because I like to imagine things out of the words I've red. It's pretty boring to know what I was doing for entertainment but I'm contended on everything that I do.           </p>
+         
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <!-- /Section: about -->
+   <section id="about" class="home-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+
+          <div class="section-heading text-center">
+            <div class="wow bounceInDown" data-wow-delay="0.2s">
+              <h2>My Favorite Application</h2>
+            </div>
+            <p class="wow bounceInUp" data-wow-delay="0.3s"><u>2019</u></p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+
+          <img src="img/youtube.png" class="img-responsive img-rounded" alt="" />
+        </div>
+        <div class="col-md-6">
+          <p><strong>Youtube</strong></p>
+          <p>
+My mostly used application so far is Youtube so I considered it as my favorite application.
+I always go to Youtube when it comes to pasting time and entertaining myself, there's no other application
+that can do it better than watching random videos on Youtube. 
+It is a video-sharing application that has the features to upload, view, rate, share, add to playlists, report, comment on videos, and subscribe to other users.
+Most of the time I start Youtube to mainly entertain and watch really interesting how to's, what if's videos,
+and eventually go into real life events such as wildlife and the cosmos. It's quite entertaining and somehow informative
+to watch videos randomly because I get to be knowledgeable while enjoying watching on things I didn't know from the first place.
+I get to access Youtube since I discovered internet because watching videos really entertains me the most, given that the internet when I was a child was overwhelming me.
+For me I think Youtube is the best application for entertaining one's self because one gets to decide what they wanted to watch, although there are restrictions for graphic contents,
+but most of the contents from the videos uploaded in Youtube doesn't have those restrictions, in fact if there are videos that contains graphic content and doesn't have restrictions
+that video can be taken down by Youtube itself for violating Youtube's rules and regulations. But overall Youtube can prove itself to be the best application in terms of entertainment, wether it would be from music, gaming, and much more contents.   </p>
+         
+        </div>
+      </div>
+    </div>
+  </section>
+	<!-- /Section: about -->
+   <section id="about" class="home-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+
+          <div class="section-heading text-center">
+            <div class="wow bounceInDown" data-wow-delay="0.2s">
+              <h2>1000 Floor Elevator</h2>
+
+        </div>
+		<p class="wow bounceInUp" data-wow-delay="0.3s"><u>Interface Design</u></p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+
+          <img src="img/gallery/elev.PNG" class="img-responsive img-rounded" alt="" />
+        </div>
+        <div class="col-md-6">
+          <p>
+			The inputs will consist of 12 buttons 10 of which are numbers to be activated directing it to the 1000 floors of the building and there is a button for basement floor. 
+			However, there is another button that has fingerprint authorization and has the capability to prioritize people, these people will only be the highest-ranking people in the infrastructure itself and the people of giving floor 
+			maintenance. These people will be the only chosen to be authorized in the priority feature of the elevator.
+		  </p>
+         
+        </div>
+      </div>
+    </div>
+  </section>
+  
+   <section id="projects" class="home-section text-center bg-gray">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <div class="wow bounceInDown" data-wow-delay="0.4s">
+            <div class="section-heading">
+              <h1>Need Finding</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+          <div class="wow bounceInUp" data-wow-delay="0.4s">
+            <h2><u>Project: Study Lock</u></h2>
+			 <div class="item"><a href="img/gallery/login.png" title="Log-in Page" data-lightbox-gallery="gallery1"><img src="img/gallery/login.png" class="img-responsive " alt="img"></a></div>
+            </div>
+          </div>
+        </div>
+		<p><strong>Problem:</strong></p>
+		<p>Smartphones tend to be distractive while learning because of the connectivity it offers. The presence of smartphones affect the focus a student as one tries to check notifications to stay connected from time to time.</p>
+		<p><strong>App Solution:</strong></p>
+		<p>Our group came up with an app idea solution for this problem which is, Study Lock. We find it very helpful to have this kind of app because all of us review our lecture notes in our mobile phones because it’s easier and paperless. Our app’s main feature is the complete lock, which will lock the phone from all of its features excluding emergency call features and partial lock, for selected apps only, for span of time set by the user. Both will be very effective as most people tend to still check their phones even without notification or any alerts receive. This will allow users to have undivided focus during study sessions.</p>
+		<p><strong>Supporting Studies:</strong></p>
+		<li>Researchers found that students sending and receiving messages while studying scored lower test results and were less effective at tasks such as note taking. It found that when students did not use mobiles, they were better at being able to recall information. (BBC)</li>
+		<li>Cell phones give students access to tools and apps that can help them complete and stay on top of their class work. These tools can also teach students to develop better study habits, like time management and organization skills. (Oxford Learning)</li>
+		<br>
+		<br>
+		<blockquote> “Our phones are not just static, there are smart people behind it trying to get our attention. Everyone is fighting for our attention to turn it into profit.” (Nikola, D. 2017)</blockquote>
+	  </div>
+	  
+	  
+    </div>
+  </section>
+  
+  <section id="projects" class="home-section text-center bg-gray">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <div class="wow bounceInDown" data-wow-delay="0.4s">
+            <div class="section-heading">
+              <h1>Initial Prototype</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+          <div class="wow bounceInUp" data-wow-delay="0.4s">
+            <h2><u>Project: Study Lock</u></h2>
+			 <div class="item"><a href="img/gallery/proto.png" title="Initial Prototype" data-lightbox-gallery="gallery1"><img src="img/gallery/proto.png" class="img-responsive " alt="img"></a></div>
+            </div>
+          </div>
+        </div>
+		<p><strong>Login Page:</strong></p>
+		<p>The application will require an authentication page to verify its only user. The user can log-in directly through their Google account or Study Luck account. If the user doesn't have an account yet, they can register by tapping the register button.</p>
+		<p><strong>Complete Lock:</strong></p>
+		<p>One of the application's main feature which will disable all the phone's functionality for a certain time only. Sliding the button will activate it and upon activating it will require its user's fingerprint authentication and desired time to lock.</p>
+		<p><strong>Partial Lock:</strong></p>
+		<li>Quite similar to the Complete Lock feature, Partial Lock puts restriction or disables functionalities for certain applications only picked by the user themselves. Upon activating it will also require fingerprint authentication and desired time to lock.</li>
+		
+	  </div>
+	  
+	  
+    </div>
+  </section>
+  
+  <section id="about" class="home-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+
+          <div class="section-heading text-center">
+            <div class="wow bounceInDown" data-wow-delay="0.2s">
+              <h2>Persona</h2>
+
+        </div>
+		 <p class="wow bounceInUp" data-wow-delay="0.3s"><u>Project: Study Lock</u></p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+
+          <img src="img/gallery/persona.PNG" class="img-responsive img-rounded" alt="" />
+        </div>
+        <div class="col-md-6">
+          <p>
+			Joe works a lot of tech stuff and very good with learing new things. Though, he struggles with staying focus on one thing.
+		  </p>
+		  <p><strong>Biggest Challenges:</strong></p>
+		  <p>Joe tends to be studying with the use of technology/devices related to his course. But because every device nowadays are having social media platforms, Joe tends to have divided focus from studying</p>
+		  <p><strong>Age:</strong></p>
+		  <li>20</li>
+		  <p><strong>Occupation:</strong></p>
+		  <li>Student</li>
+		  <p><strong>Degree:</strong></p>
+		  <li>BS Information Technology</li>
+		  
+         
+        </div>	
+      </div>
+    </div>
+  </section>
+  
+  <section id="about" class="home-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+
+          <div class="section-heading text-center">
+            <div class="wow bounceInDown" data-wow-delay="0.2s">
+              <h2>Information Design</h2>
+
+        </div>
+			 <p class="wow bounceInUp" data-wow-delay="0.3s"><u>Own Design</u></p>
+			 <div class="item"><a href="img/gallery/invi.png" title="Invitation" data-lightbox-gallery="gallery1"><img src="img/gallery/invi.png" class="img-responsive " alt="img"></a></div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+
+          <img src="img/gallery/infodesign.png" class="img-responsive img-rounded" alt="" />
+        </div>
+        <div class="col-md-6">
+			<p><strong>This activity is to improve an awfully designed invitation by answering 2 questions and creating my own invitation design. Below are the questions with my answers:</strong></p>
+			<p>The image on the left represents my own invitation design.</p>
+          <p><strong>Question 1:</strong></p>
+		  <p><strong>How can we improve this?</strong></p>
+		  <li>Division of each information.</li>
+		  <li>Design related to the subject.</li>
+		  <li>Highlights of the most important information.</li>
+		<p><strong>Question 2:</strong></p>
+		<p><strong>What could be the objective function?</strong></p>
+		 <li>The given statement is a party invitation therefore it should contain the details of the location, the event, the date and time in order to inform the receiver that they are chosen to be invited.</li>
+         
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  
+  <section id="gallery" class="home-section text-center bg-gray">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <div class="wow bounceInDown" data-wow-delay="0.4s">
+            <div class="section-heading">
+              <h2>Grids</h2>
+            </div>
+			 <p class="wow bounceInUp" data-wow-delay="0.3s"><u>This activity is to visualize pages with grids.</u></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+          <div class="wow bounceInUp" data-wow-delay="0.4s">
+            <div id="owl-works" class="owl-carousel">
+              <div class="item"><a href="img/gallery/3x3empty.png" title="3x3 Grid" data-lightbox-gallery="gallery1"><img src="img/gallery/3x3empty.png" class="img-responsive" alt="img"></a>This is a 3x3 grid design consisting dark and light color shapes. Which dark color shapes represents images and light color shapes represents texts.</div>
+              <div class="item"><a href="img/gallery/3x3.png" title="3x3 Grid with Pictures" data-lightbox-gallery="gallery1"><img src="img/gallery/3x3.png" class="img-responsive " alt="img"></a>This is a 3x3 grid design consisting actual images.</div>
+			  <div class="item"><a href="img/gallery/4x4empty.png" title="4x4 Grid" data-lightbox-gallery="gallery1"><img src="img/gallery/4x4empty.png" class="img-responsive " alt="img"></a>This is a 4x4 grid design consisting dark and light color shapes. Which dark color shapes represents images and light color shapes represents texts.</div>
+			  <div class="item"><a href="img/gallery/4x4.png" title="4x4 Grid with Pictures" data-lightbox-gallery="gallery1"><img src="img/gallery/4x4.png" class="img-responsive " alt="img"></a>This is 4x4 grid design consisting actual images.</div>
+			   <div class="item"><a href="img/gallery/12-column_grid.png" title="12 Column Grid" data-lightbox-gallery="gallery1"><img src="img/gallery/12-column_grid.png" class="img-responsive " alt="img"></a>This is a 12-column grid design consisting dark and light color shapes. Which dark color shapes represents images and light color shapes represents texts.</div>
+
+			
+			</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+   
+
+  <!-- Core JavaScript Files -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/jquery.scrollTo.js"></script>
+  <script src="js/stellar.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/nivo-lightbox.min.js"></script>
+  <!-- Custom Theme JavaScript -->
+  <script src="js/custom.js"></script>
+  <script src="contactform/contactform.js"></script>
+
+</body>
+
+</html>
